f71a5d0417f1ff5c3a73e3135d8b61cf954d6239
cowalla <rmiles957@gmail.com>
2024-07-24 12:23:48 +0300

    [Fix]: No of args

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
-    "asciiweb/handlers"
+	"asciiweb/handlers"
 )
 
 func main() {
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static")))) // add static folder to serve favicon and styles
+	if len(os.Args) != 1 {
+		fmt.Println("Usage: go run .")
+		return
+	}
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-    // Handle all requests to the root path
-    http.HandleFunc("/", handlers.HomeHandler)
+	// Handle all requests to the root path
+	http.HandleFunc("/", handlers.HomeHandler)
 
-    // Start the server on port 8080
-    log.Println("Server started on :8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatalf("Could not start server: %s\n", err.Error())
-    }
+	http.HandleFunc("/ascii-art", handlers.AsciiArtHandler)
+
+	port := ":8000"
+	// Start the server on port 8080
+	log.Printf("Server started on http://localhost%s", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
 }

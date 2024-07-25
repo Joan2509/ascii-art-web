53377ac9e8db0987397b201b0fcf18c1209eed58
Cherrypick14 <cherrypicklet@gmail.com>
2024-07-25 09:50:32 +0300

    Error handling

--- asciiart/art.go
+++ asciiart/art.go
@@ -1,24 +1,19 @@
 package asciiart
 
-import (
-	"fmt"
-	"os"
-)
-
 // reads a banner file, creates a map of ASCII art, validates user input,
 // and prints the corresponding ASCII art to the console.
 func Art(inputText, banner string) (string, error) {
 	bannerFile, err := ReadBannerFile("banners/" + banner + ".txt")
-	check(err)
-	ASCIIArtMap := MapCreator(string(bannerFile))
-	asciiArt, err := ArtRetriever(inputText, ASCIIArtMap)
-	check(err)
-	return asciiArt, nil
-}
-
-func check(e error) {
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", e)
-		os.Exit(1)
+	if err != nil {
+		return "404", err
 	}
+
+	ASCIIArtMap := MapCreator(string(bannerFile))
+
+	asciiArt, err := ArtRetriever(inputText, ASCIIArtMap)
+	if err != nil {
+		return "400", err
+	}
+
+	return asciiArt, nil
 }
--- asciiart/retrieveart.go
+++ asciiart/retrieveart.go
@@ -1,7 +1,6 @@
 package asciiart
 
 import (
-	"fmt"
 	"strings"
 )
 
@@ -17,7 +16,6 @@ func ArtRetriever(s string, m map[rune][]string) (string, error) {
 
 	for ind := 0; ind < len(lines); ind++ {
 		if lines[ind] == "" {
-
 			result.WriteString("\n")
 		} else {
 			for j := 0; j < 8; j++ {
@@ -25,9 +23,6 @@ func ArtRetriever(s string, m map[rune][]string) (string, error) {
 					if asciiArt, ok := m[char]; ok {
 						// Add the corresponding ASCII art for the character
 						result.WriteString(asciiArt[j])
-					} else {
-						// For cases of non-ascii characters
-						return "", fmt.Errorf("error! invalid input: %s", string(char))
 					}
 				}
 				result.WriteString("\n")
